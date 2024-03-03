5341c2228a17a0b43a273f1f54a24ebe57fd62b5
Diego Sanchez Briales <105086398+Diegobrls@users.noreply.github.com>
2024-03-04 00:19:16 +0100

    Add files via upload

---
+++ app/Models/Cestas.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cestas extends Model
+{
+    use HasFactory;
+    //relacion 1 a muchos con pedidos
+    public function pedidos() {
+        return $this->hasMany(Pedidos::class);
+    }
+    //relacion mucho a mucho con elementos
+    public function elementos() {
+        return $this->belongsToMany(Elementos::class, 'elementos_cesta', 'cesta_id', 'elemento_id');
+    }
+}
